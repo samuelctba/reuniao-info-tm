afa46e2f128878172d30497e267c26a1a4bd4b43
Samuel A Camargo <samuelctba@gmail.com>
2019-02-06 09:14:50 -0200

    trying to make team git works in eclipse

--- src/main/java/tm/info/reuniao/repositorio/IUsuarioServico.java
+++ src/main/java/tm/info/reuniao/repositorio/IUsuarioServico.java
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import tm.info.reuniao.model.Usuario;
 
+//@RepositoryRestResourse from JPA
 public interface IUsuarioServico extends MongoRepository<Usuario, String> {
 	
 	Usuario findByUsuario(String usuario);
